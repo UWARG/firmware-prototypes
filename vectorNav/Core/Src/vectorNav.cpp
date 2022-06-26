381ac63fca832ef29dca4102530393bcdccdc608
Christopher Chung <tps90067@gmail.com>
2022-06-26 12:37:26 -0400

    add skeleton functions in vectorNav.cpp file

--- vectorNav/Core/Src/vectorNav.cpp
+++ vectorNav/Core/Src/vectorNav.cpp
@@ -6,3 +6,32 @@
  */
 
 #include "vectorNav.hpp"
+
+/* Public Methods  ---------------------------------------------------------*/
+
+Position& VN300::getInstance(){
+    static VN300 singleton;
+    return singleton;
+}
+
+void VN300::GetResult(PositionData_t* data){
+
+}
+
+/* Private methods  ---------------------------------------------------------*/
+VN300::VN300(){
+    VN300Init();
+}
+
+void VN300::VN300Init(void){
+
+}
+
+void VN300::getGPSData(GPSData_t& gpsData){
+
+}
+
+void VN300::getIMUData(IMUData_t& imuData){
+
+}
+
