c91815425255db8dbaa2631da64867300568b1aa
Christopher Chung <tps90067@gmail.com>
2022-06-26 12:13:20 -0400

    Create bare minimum class for VN300 and some structs -added PositionData_t struct which will hold ALL data and be handled with GetResult -created Position class which is both IMU and GPS data -created VN300 class which inherits Position for VN300 operation
    TODO: figure out with helper functions are required for the VN300
    operation

--- vectorNav/Core/Inc/vectorNav.hpp
+++ vectorNav/Core/Inc/vectorNav.hpp
@@ -11,10 +11,9 @@
 #include "stm32f0xx_hal.h"
 #include <stdint.h>
 
+// Header strings that are sent to the VN300 to request data from it
 #define GPS_HEADER "$VNGPS"
 #define IMU_HEADER "$VNIMU"
-
-#define 
 
 // struct based on section 8.8.1 here: https://geo-matching.com/uploads/default/m/i/migrationca8kud.pdf
 typedef struct{
@@ -33,7 +32,7 @@ typedef struct{
     float vertAcc;      // Vertical position accuracy estimate in m
     float speedAcc;     // Speed accuracy estimate in m/s
     float timeAcc;      // Time accuracy estimate in seconds
-}GPS_data_t; 
+}GPSData_t; 
 
 // struct based on section 1.7.7 here: https://geo-matching.com/uploads/default/m/i/migrationca8kud.pdf
 typedef struct{
@@ -48,6 +47,47 @@ typedef struct{
     float GyroZ;        // Uncompensated Angular rate Z-axis in rad/s
     float temp;         // IMU Temperature in degrees C
     float pressure;     // Barometric pressure in kPa
-}IMU_data_t;
+}IMUData_t;
+
+// this is the enum that will signify which data to send
+typedef enum {
+    gpsImu = 0,
+    gps,
+    imu
+}typeData_e;
+
+/*
+    -this is the struct that will be passed in and out of GetResult
+    -based on type_data, we will know which data to send with all the rest being 0
+*/
+typedef struct{
+    GPSData_t gps_data;
+    IMUData_t imu_data;
+    typeData_e type_data;
+}PositionData_t
+
+
+class Position{
+    public:
+        virtual void GetResult(PositionData_t& data) = 0;
+};
+
+class VN300: public Position{
+    public:
+        static Position& getInstance();
+
+        VN300(const VN300*)=delete;
+
+        void GetResult(PositionData_t& data);
+
+    private:
+
+        VN300();
+
+        void VN300Init(void); //we may not need this
+
+        //helper functions ...
+
+};
 
 #endif /* INC_VECTORNAV_HPP_ */
