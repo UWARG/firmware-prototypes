fee875b95bf80fe2f2af77d11625b52c2e96cc10
Christopher Chung <tps90067@gmail.com>
2022-07-12 22:14:10 -0400

    redefine gps and imu structs, populating functions

--- vectorNav/Core/Inc/vectorNav.hpp
+++ vectorNav/Core/Inc/vectorNav.hpp
@@ -8,53 +8,38 @@
 #ifndef INC_VECTORNAV_HPP_
 #define INC_VECTORNAV_HPP_
 
-#include "stm32f0xx_hal.h"
+#include "main.h"
 #include <stdint.h>
 
 // Header strings that are sent to the VN300 to request data from it
-#define GPS_HEADER "$VNGPS"
-#define IMU_HEADER "$VNIMU"
 
-// struct based on section 8.8.1 here: https://geo-matching.com/uploads/default/m/i/migrationca8kud.pdf
-typedef struct{
-    double time;        // GPS time of week in seconds
-    uint16_t week;      // GPS week
-    uint8_t gpsFix;     // GPS fix type
-    uint8_t numSats;    // Number of GPS satellites used in solution
-    double latitude;    // Latitude in degrees
-    double longitude;   // Longitude in degrees
-    double altitude;    // Altitude above ellipsoid
-    float nedVelX;      // Velocity measurement in north direction in m/s
-    float nedVelY;      // Velocity measurement in east direction in m/s
-    float nedVelZ;      // Velocity measurement in down direction in m/s
-    float northAcc;     // North position accuracy estimate in m
-    float eastAcc;      // East position accuracy estimate in m
-    float vertAcc;      // Vertical position accuracy estimate in m
-    float speedAcc;     // Speed accuracy estimate in m/s
-    float timeAcc;      // Time accuracy estimate in seconds
-}GPSData_t; 
+const char* GPS_HEADER = "$VNGPS";
+const char* IMU_HEADER = "$VNIMU";
 
-// struct based on section 1.7.7 here: https://geo-matching.com/uploads/default/m/i/migrationca8kud.pdf
 typedef struct{
-    float magX;         // Uncompensated Magnetic X-axis in Gauss
-    float magY;         // Uncompensated Magnetic Y-axis in Gauss
-    float magZ;         // Uncompensated Magnetic Z-axis in Gauss
-    float AccelX;       // Uncompensated Acceleration X-axis in m/s^2
-    float AccelY;       // Uncompensated Acceleration Y-axis in m/s^2
-    float AccelZ;       // Uncompensated Acceleration Z-axis in m/s^2
-    float GyroX;        // Uncompensated Angular rate X-axis in rad/s
-    float GyroY;        // Uncompensated Angular rate Y-axis in rad/s
-    float GyroZ;        // Uncompensated Angular rate Z-axis in rad/s
-    float temp;         // IMU Temperature in degrees C
-    float pressure;     // Barometric pressure in kPa
+    uint64_t time;        // Time since startup in nanoseconds
+    double latitude;      // Latitude in degrees
+    double longitude;     // Longitude in degrees
+    double altitude;      // Altitude in m
+    float velNorth;       // Velocity in North frame in m/s
+    float velEast;        // Velocity in East frame in m/s
+    float velDown;        // Velocity in Down frame in m/s
+}GPSData_t;
+
+typedef struct{
+    float pitch;        // Pitch in degrees
+    float roll;         // Roll in degrees
+    float yaw;          // Yaw in degrees
+    float accel0;       // Acceleration in m/s^2 in body frame 0
+    float accel1;       // Acceleration in m/s^2 in body frame 1
+    float accel2;       // Acceleration in m/s^2 in body frame 2
 }IMUData_t;
 
 // This is the enum that will signify which data to send
-typedef enum {
-    gpsImu = 0,
-    gps,
-    imu
-}typeData_e;
+typedef struct {
+    bool gps;
+    bool imu;
+}requestData_t;
 
 /*
     -This is the struct that will be passed in and out of GetResult
@@ -63,10 +48,10 @@ typedef enum {
 typedef struct{
     GPSData_t gps_data;
     IMUData_t imu_data;
-    typeData_e type_data;
-}PositionData_t
+    requestData_t req_data;
+}PositionData_t;
 
-class Position{
+class Position {
     public:
         virtual void GetResult(PositionData_t& data) = 0;
 };
@@ -112,12 +97,27 @@ class VN300: public Position{
          */
         void getGPSData(GPSData_t& gpsData);
 
+        void parse_gps_data(GPSData_t& gpsData);
+
+        float binary_to_float(uint64_t val);
+
         /**
          * @brief Gets the IMU data from the VN300
          * 
          * @param imuData The struct holding the IMU data
          */
         void getIMUData(IMUData_t& imuData);
+        
+        /**
+         * @brief Sends an ASCII command through UART
+         * 
+         * @param command ASCII command string 
+         */
+
+        void parse_imu_data(IMUData_t& imuData);
+        void sendCommand(const char* command, uint16_t size);
+
+        void recieveResponse(void);
 
 };
 
--- vectorNav/Core/Src/vectorNav.cpp
+++ vectorNav/Core/Src/vectorNav.cpp
@@ -7,6 +7,13 @@
 
 #include "vectorNav.hpp"
 
+extern UART_HandleTypeDef huart3;
+
+const char MSG_CONFIG_COMMAND[] = "$VNWRG,75,0,0,01,1C9*XX"; //ASCII command to configure custom binary data group 1
+const char POLL_DATA_COMMAND[] = "$VNBOM,1*XX";              //ASCII command to poll VN300 and request custom binary data group 1
+const uint16_t PAYLOAD_SIZE = 74;                             //Size of custom message payload coming from VN300
+uint8_t payload_data[PAYLOAD_SIZE] = {0};                         //RX buffer to hold incoming data
+
 /* Public Methods  ---------------------------------------------------------*/
 
 Position& VN300::getInstance(){
@@ -14,8 +21,14 @@ Position& VN300::getInstance(){
     return singleton;
 }
 
-void VN300::GetResult(PositionData_t* data){
-
+void VN300::GetResult(PositionData_t& data){
+    //set all values in the struct to 0?
+    
+    if (data.req_data.gps)
+        getGPSData(data.gps_data);
+    
+    if (data.req_data.imu)
+        getIMUData(data.imu_data);
 }
 
 /* Private methods  ---------------------------------------------------------*/
@@ -24,14 +37,109 @@ VN300::VN300(){
 }
 
 void VN300::VN300Init(void){
-
+    //send satellite orientation commands
+    //set configuration register with custom binary output
+    sendCommand(MSG_CONFIG_COMMAND, sizeof(MSG_CONFIG_COMMAND));
 }
 
 void VN300::getGPSData(GPSData_t& gpsData){
+    sendCommand(POLL_DATA_COMMAND, sizeof(POLL_DATA_COMMAND));
+    recieveResponse();
+    parse_gps_data(gpsData);
+}
+
+void VN300::parse_gps_data(GPSData_t& gpsData){
+    //Define offsets
+    const uint8_t TIME_OFFSET = 4;
+    const uint8_t LATITUDE_OFFSET = 24;
+    const uint8_t LONGITUDE_OFFSET = 32;
+    const uint8_t ALTITUDE_OFFSET = 40;
+    const uint8_t VELNORTH_OFFSET = 48;
+    const uint8_t VELEAST_OFFSET = 52;
+    const uint8_t VELDOWN_OFFSET = 56;
+
+    uint64_t temp = 0;
+
+    for (uint8_t i = 7; i >= 0; i--)
+        gpsData.time <<= payload_data[TIME_OFFSET + i];
+
+    for (uint8_t i = 7; i >= 0; i--)
+        temp <<= payload_data[LATITUDE_OFFSET + i];
+    gpsData.latitude = binary_to_float(temp);
+
+    for (uint8_t i = 7; i >= 0; i--)
+        temp <<= payload_data[LONGITUDE_OFFSET + i];
+    gpsData.longitude = binary_to_float(temp);
+
+    for (uint8_t i = 7; i >= 0; i--)
+        temp <<= payload_data[ALTITUDE_OFFSET + i];
+    gpsData.altitude = binary_to_float(temp);
+
+    for (uint8_t i = 3; i >= 0; i--)
+        temp <<= payload_data[VELNORTH_OFFSET + i];
+    gpsData.velNorth = binary_to_float(temp);
+
+    for (uint8_t i = 3; i >= 0; i--)
+        temp <<= payload_data[VELEAST_OFFSET + i];
+    gpsData.velEast = binary_to_float(temp);
+
+    for (uint8_t i = 3; i >= 0; i--)
+        temp <<= payload_data[VELDOWN_OFFSET + i];
+    gpsData.velDown = binary_to_float(temp);
+    
+}
+
+float VN300::binary_to_float(uint64_t val){
 
 }
 
 void VN300::getIMUData(IMUData_t& imuData){
 
 }
+
+void VN300::parse_imu_data(IMUData_t& imuData){
+    const uint8_t PITCH_OFFSET = 12;
+    const uint8_t ROLL_OFFSET = 16;
+    const uint8_t YAW_OFFSET = 20;
+    const uint8_t ACCEL0_OFFSET = 60;
+    const uint8_t ACCEL1_OFFSET = 64;
+    const uint8_t ACCEL2_OFFSET = 68;
+
+    uint64_t temp = 0;
+
+    for (uint8_t i = 3; i >= 0; i--)
+        temp <<= payload_data[PITCH_OFFSET + i];
+    imuData.pitch = binary_to_float(temp);
+
+    for (uint8_t i = 3; i >= 0; i--)
+        temp <<= payload_data[ROLL_OFFSET + i];
+    imuData.roll = binary_to_float(temp);
+
+    for (uint8_t i = 3; i >= 0; i--)
+        temp <<= payload_data[YAW_OFFSET + i];
+    imuData.yaw = binary_to_float(temp);
+
+    for (uint8_t i = 3; i >= 0; i--)
+        temp <<= payload_data[ACCEL0_OFFSET + i];
+    imuData.accel0 = binary_to_float(temp);
+
+    for (uint8_t i = 3; i >= 0; i--)
+        temp <<= payload_data[ACCEL1_OFFSET + i];
+    imuData.accel1 = binary_to_float(temp);
+
+    for (uint8_t i = 3; i >= 0; i--)
+        temp <<= payload_data[ACCEL2_OFFSET + i];
+    imuData.accel2 = binary_to_float(temp);
+}
+
+
+
+void VN300::sendCommand(const char* command, uint16_t size){
+    HAL_UART_Transmit(&huart3, (uint8_t*)command, size, 100);
+}
+
+void VN300::recieveResponse(void){
+    HAL_UART_Receive(&huart3, payload_data, PAYLOAD_SIZE, 100);
+}
+
 
